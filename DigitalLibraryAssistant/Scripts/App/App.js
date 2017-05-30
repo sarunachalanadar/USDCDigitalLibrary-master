ccf2e1e658f44aa11c0170b5ba333a1aa64a6541
snicolais <snicolais@USCMPSNICOLAIS1>
2017-05-30 11:48:31 -0400

    Add project files.

---
+++ DigitalLibraryAssistant/Scripts/App/App.js
@@ -0,0 +1,28 @@
+﻿var app = angular.module("app", ["ngRoute"]);
+app.config(function ($routeProvider, $locationProvider) {
+    var templatesDir = "/Pages/";
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $routeProvider
+    .when("/", {
+        templateUrl: templatesDir + "Home.html",
+        controller: "mainController"
+    })
+    .when("/home", {
+        templateUrl: templatesDir + "Home.html",
+        controller: "mainController"
+    })
+    .when("/test", {
+        templateUrl: templatesDir + "Test.html",
+        controller: "mainController"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+});
+
+app.controller('mainController', function ($scope) {
+    $scope.message = "This message is from the controller";
+});
